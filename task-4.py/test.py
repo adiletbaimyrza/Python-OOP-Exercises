6361b3e544981fdda711af3f3d9fc98e813285aa
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-07-20 18:17:01 +0200

    task4 completed, tests passed

---
+++ task-4.py/test.py
@@ -0,0 +1,15 @@
+import unittest
+from main import Solution
+
+
+class testTask_4(unittest.TestCase):
+    def test_update_counters(self):
+        for i in range(40):
+            Solution.update_counters()
+        
+        from main import counter, dot_counter
+        self.assertEqual(40, counter)
+        self.assertEqual('........................................', dot_counter)
+        
+if __name__ == "__main__":
+    unittest.main()
